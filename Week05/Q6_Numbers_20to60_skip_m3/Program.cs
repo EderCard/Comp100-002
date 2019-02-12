aa8351561338b26a7dadf9f8c13e9f7275fb8ded
EderCard <edersoncard@gmail.com>
2019-02-11 23:12:26 -0500

    Week05 Q1 to Q6

---
+++ Week05/Q6_Numbers_20to60_skip_m3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 6.	To display numbers 20 … 60 on separate lines skipping the multiple of 3. 
+ *      You must print a blank line for each multiple.
+ */
+namespace Q6_Numbers_20to60_skip_m3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initializarer
+            int start = 20, end = 60;
+            do
+            {
+                if (start % 3 == 0)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine($"{start}");
+                }
+                start = start + 1; //updater
+            } while (start <= end); //checker
+        }
+    }
+}
